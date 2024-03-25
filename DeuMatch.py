a6612f5c437dc0a0a761801cb1faec6d35aee319
Nilton Cesar da Costa Silva <niltondemariadev@gmail.com>
2024-03-25 09:45:04 -0300

    Função de busca dos candidatos

--- DeuMatch.py
+++ DeuMatch.py
@@ -1,2 +1,19 @@
 candidatosLista = [[5, 10, 8, 8], [10, 7, 7, 8], [8, 5, 4, 9], [2, 2, 2, 1], [10, 10, 8, 9]]
 
+def buscaCandidatos(e, t, p, s):
+    
+    tamanhoLista = len(candidatosLista)
+    b = 0
+
+    for n in range(tamanhoLista):
+        if (candidatosLista[n][0] >= e and
+            candidatosLista[n][1] >= t and
+            candidatosLista[n][2] >= p and
+            candidatosLista[n][3] >= s):
+
+            candidato = n + 1
+            print(f"   Candidato {candidato} atende o critério.")
+            b += 1
+    if b == 0:
+        print("   Nenhuma candidato atente ao critério.")
+
